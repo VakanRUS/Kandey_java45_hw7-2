e660bf2a2ac0bc246f276d9d37893226e55d4f9b
Vakan <vakan1@yandex.ru>
2025-05-11 12:57:05 +0300

    done homework

---
+++ src/Author.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class Author {
+    private String nameFirst;
+    private String nameLast;
+
+    public Author(String nameFirst, String nameLast) {
+        this.nameFirst = nameFirst;
+        this.nameLast = nameLast;
+    }
+
+    public String toString() {
+        return nameFirst + " " + nameLast;
+    }
+
+    public String getNameFirst() {
+        return nameFirst;
+    }
+
+    public String getNameLast() {
+        return nameLast;
+    }
+
+    @Override
+    public boolean equals(Object isEquals) {
+        if (isEquals == null || getClass() != isEquals.getClass()) return false;
+        Author author = (Author) isEquals;
+        return Objects.equals(nameFirst, author.nameFirst) && Objects.equals(nameLast, author.nameLast);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameFirst, nameLast);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Сергей", "Лукьяненко");
+        Author author2 = new Author("Стивен", "Кинг");
+        Book book1 = new Book("Лабиринт отражений", 1997, author1);
+        Book book2 = new Book("Тёмная башня", 1975, author2);
+        book2.setYearOfPublish(1982);
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
